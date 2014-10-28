f8229ae52fb4fee1fd18112452c2a5953df90e7e
Mike Kornelson <mike@koretech.ca>
2014-10-28 14:44:58 -0500

    Fixed a package bug and updated packaging

---
+++ package.js
@@ -0,0 +1,28 @@
+var client = 'client', server = 'server', both = ['client', 'server'];
+
+Package.describe({
+	name: 'krt:rome',
+	summary: 'Rome Date and Time Picker',
+	version: '1.2.0_1',
+	git: 'https://github.com/koretech/meteor-krt-rome.git'
+});
+
+Package.onUse(function(api){
+
+	api.versionsFrom('METEOR@0.9.4');
+
+	api.use([
+		'templating',
+		'underscore',
+		'less',
+		'mquandalle:bower@0.1.11'
+	], both);
+
+	api.addFiles([
+		'bower.json',
+		'lib/rome/dist/rome.css',
+		'lib/rome/dist/rome.standalone.js',
+		'rome.less'
+	], client);
+
+});
