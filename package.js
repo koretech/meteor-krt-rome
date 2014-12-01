94d47bc3f6bbad7723732bbba7deb55f129084de
Mike Kornelson <mike@koretech.ca>
2014-12-01 10:37:25 -0600

    Upgraded to Meteor 1.0

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:rome',
 	summary: 'Rome Date and Time Picker',
-	version: '1.2.0_1',
+	version: '1.2.0_2',
 	git: 'https://github.com/koretech/meteor-krt-rome.git'
 });
 
 Package.onUse(function(api){
 
-	api.versionsFrom('METEOR@0.9.4');
+	api.versionsFrom('METEOR@1.0');
 
 	api.use([
 		'templating',
